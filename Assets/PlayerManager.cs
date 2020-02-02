c6d65ddd935c7537d4cedd1faa5d97bdd91a7966
Pâmela Beltrani <beltrani_assis@hotmail.com>
2020-02-02 01:11:11 -0300

    Point Count
    Added the point count

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    ParticleSystem ps;
+    public int collectedOrbs = 0;
+    public float orbsMultiplier = 1.0f;
+    float initialParticleEmissionCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponentInChildren<ParticleSystem>();
+        initialParticleEmissionCount = ps.emission.rateOverTime.constant;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CollectOrb()
+    {
+        collectedOrbs++;
+        ps.emissionRate = collectedOrbs * orbsMultiplier + initialParticleEmissionCount;
+    }
+}
---
+++ Assets/PointManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerManager playerManager = other.GetComponent<PlayerManager>();
+            playerManager.CollectOrb();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(this.gameObject, 0.1f);
+    }
+}
